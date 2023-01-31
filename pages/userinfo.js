5ea2e3da7b0907d2f7d57a29fcfef932aa449b0e
규민 박 <pkm229125@naver.com>
2023-01-31 12:42:04 +0900

    error fix(01.31)

--- pages/userinfo.js
+++ pages/userinfo.js
@@ -19,7 +19,7 @@ export default function Userinfo({memebers,memberNames})
                 </Head>
 
                   
-                <div className="grid grid-cols-2 md:grid-cols-3 py-10 m-6 gap-8 ">
+                <div className="grid grid-cols-1 md:grid-cols-3 py-10 m-6 gap-8 ">
                   {memebers.results.map((allProject) => (
                     // <h1>{allProject.properties.Name.rich_text[0].plain_text}</h1>
                     <MemberItem key={allProject.id}data={allProject}/>
